094fd082a23677990c9458800c7a4b0fa2f56ffc
sanggu <asg0310@inissoft.com>
2023-08-14 10:31:35 +0900

    step0

---
+++ src/main/java/step0/GapString.java
@@ -0,0 +1,15 @@
+package step0;
+
+import java.util.Scanner;
+
+public class GapString {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.println(a+b);
+	}
+
+}
